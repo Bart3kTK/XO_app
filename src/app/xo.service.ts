4484b12e5e077f69a179058583b33ba1ba6ee97e
Bart3kTK <bkamieniarz55@wp.pl>
2024-06-28 15:41:51 +0200

    update

--- src/app/xo.service.ts
+++ src/app/xo.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { game } from './game';
 
 @Injectable({
   providedIn: 'root'
 })
 export class XoService {
-  private baseUrl = 'http://localhost:8080'; 
+  private baseUrl = 'http://localhost:8080/XO'; 
 
   constructor(private http: HttpClient) {}
 
-  getExampleData(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/example-endpoint`);
+  getData(): Observable<game[]> {
+    return this.http.get<game[]>(`${this.baseUrl}/games`);
   }
 
-  postExampleData(data: any): Observable<any> {
-    return this.http.post(`${this.baseUrl}/example-endpoint`, data);
-  }
+//   postExampleData(data: any): Observable<any> {
+//     return this.http.post(`${this.baseUrl}/example-endpoint`, data);
+//   }
 }
